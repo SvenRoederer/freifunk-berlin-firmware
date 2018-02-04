bbb46f413ca8bfe104f1002d5ca19b52fa85a9b3
Sven Roederer <freifunk@it-solutions.geroedel.de>
2018-02-04 02:04:44 +0100

    config: set default-package to "SAm0815"

--- config.mk
+++ config.mk
@@ -1,7 +1,7 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ar71xx-generic
-PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
+PACKAGES_LIST_DEFAULT=SAm0815
 OPENWRT_SRC=https://git.openwrt.org/openwrt/openwrt.git
 OPENWRT_COMMIT=33732f4a9c17921b782167a0dcaba9703d4e6753
 SET_BUILDBOT=env
