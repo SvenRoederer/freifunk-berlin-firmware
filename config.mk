ad17321c64304acd90d1ef40b202781988872020
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-04-27 16:23:29 +0200

    also build "non-olsr" image-flavor

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ath79-generic
-PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual
+PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual non-olsr
 MAKE_ARGS=V=s
