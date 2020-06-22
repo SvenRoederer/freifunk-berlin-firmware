810cb7f6eb85e72cc90772e9a286e418a70a509d
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-06-23 00:08:20 +0200

    config: drop creating images of flavor "manual" in favor of "non-olsr"
    OLSRv1 actually seems dead, so look for another future.

--- config.mk
+++ config.mk
@@ -1,5 +1,5 @@
 # default parameters for Makefile
 SHELL:=$(shell which bash)
 TARGET=ath79-generic
-PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger manual non-olsr
+PACKAGES_LIST_DEFAULT=notunnel tunnel-berlin-tunneldigger non-olsr
 MAKE_ARGS=V=s
