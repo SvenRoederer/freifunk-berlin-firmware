04a52d6a17b554fe20d27982c40474173e44f29e
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-10-16 00:19:15 +0200

    comment out and strip down

--- embedded-files/lib/update/freifunk-berlin_freeup-ram.sh
+++ embedded-files/lib/update/freifunk-berlin_freeup-ram.sh
@@ -1,24 +1,31 @@
-echo >&2 "This is the Freifunk Berlin-script"
+#echo >&2 "This is the Freifunk Berlin-script"
 
-MEMLIMIT=33554432   # 32MB
-SERVICES="cron pingcheck uhttpd rpcd odhcpd"        
+#MEMLIMIT=33554432   # 32MB
+SERVICES="cron pingcheck uhttpd"
 
-MEMFREE=$(cat /proc/meminfo |grep MemTotal|awk '{print $2}')
+#MEMFREE=$(cat /proc/meminfo |grep MemTotal|awk '{print $2}')
 
-ifdown ffuplink
+#MEM=$(cat /proc/meminfo | head -n 3)
+#echo >&2 "mem before: ${MEM}"
+
+#ifdown ffuplink
 
 #echo stopping non-essential services
 for service in $SERVICES; do
-  echo >&2 -n "${service} "
-  [ -x /etc/init.d/${service} ] && /etc/init.d/${service} running && { /etc/init.d/${service} stop; echo >&2 -n " ..stopped.. "; }
+#  echo >&2 -n "${service} "
+  [ -x /etc/init.d/${service} ] && { /etc/init.d/${service} running && /etc/init.d/${service} stop; }
 done
-echo >&2 ""
+#echo >&2 ""
 
-echo >&2 flushing files from /tmp
+#echo >&2 flushing files from /tmp
 rm >/dev/null -f /tmp/luci-indexcache*
 rm >/dev/null -rf /tmp/luci-modulecache
 
+#MEM=$(cat /proc/meminfo | head -n 3)
+#echo >&2 "mem after: ${MEM}"
 
-echo >&2 end of Freifunk Berlin-script
+#echo >&2 "ps: " $(ps w)
+
+#echo >&2 end of Freifunk Berlin-script
 #exit 10
 
