490b8c0018d57f7c13a737e1ab923df595f12d64
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-10-16 00:37:36 +0200

    put into functioni to aviod variable clashes

--- embedded-files/lib/update/freifunk-berlin_freeup-ram.sh
+++ embedded-files/lib/update/freifunk-berlin_freeup-ram.sh
@@ -1,31 +1,20 @@
-#echo >&2 "This is the Freifunk Berlin-script"
 
-#MEMLIMIT=33554432   # 32MB
-SERVICES="cron pingcheck uhttpd"
+ffberlin_freeup_ram() {
+  local SERVICES
+  local service
 
-#MEMFREE=$(cat /proc/meminfo |grep MemTotal|awk '{print $2}')
+  SERVICES="cron pingcheck uhttpd"
 
-#MEM=$(cat /proc/meminfo | head -n 3)
-#echo >&2 "mem before: ${MEM}"
+  # stop services if existing and running
+  for service in $SERVICES; do
+    [ -x /etc/init.d/${service} ] && { /etc/init.d/${service} running && /etc/init.d/${service} stop; }
+  done
 
-#ifdown ffuplink
+  # remove LuCI-caches
+  rm >/dev/null -f /tmp/luci-indexcache*
+  rm >/dev/null -rf /tmp/luci-modulecache
 
-#echo stopping non-essential services
-for service in $SERVICES; do
-#  echo >&2 -n "${service} "
-  [ -x /etc/init.d/${service} ] && { /etc/init.d/${service} running && /etc/init.d/${service} stop; }
-done
-#echo >&2 ""
+}
 
-#echo >&2 flushing files from /tmp
-rm >/dev/null -f /tmp/luci-indexcache*
-rm >/dev/null -rf /tmp/luci-modulecache
-
-#MEM=$(cat /proc/meminfo | head -n 3)
-#echo >&2 "mem after: ${MEM}"
-
-#echo >&2 "ps: " $(ps w)
-
-#echo >&2 end of Freifunk Berlin-script
-#exit 10
+ffberlin_freeup_ram
 
