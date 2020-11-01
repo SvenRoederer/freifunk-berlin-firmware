0ed31c2126084465b2d86d23892dca165e402488
Sven Roederer <freifunk@it-solutions.geroedel.de>
2020-11-01 01:55:32 +0100

    freifunk-berlin_freeup-ram: reorganize

--- embedded-files/lib/update/freifunk-berlin_freeup-ram.sh
+++ embedded-files/lib/update/freifunk-berlin_freeup-ram.sh
@@ -3,7 +3,10 @@ ffberlin_freeup_ram() {
   local SERVICES
   local service
 
-  SERVICES="cron pingcheck uhttpd"
+  ifdown ffuplink
+  ifdown tunl0
+
+  SERVICES="cron pingcheck uhttpd rpcd urngd rssi firewall dnsmasq"
 
   # stop services if existing and running
   for service in $SERVICES; do
@@ -14,14 +17,22 @@ ffberlin_freeup_ram() {
   rm >/dev/null -f /tmp/luci-indexcache*
   rm >/dev/null -rf /tmp/luci-modulecache
 
-  # remove unused kernel modules
-  [ -x lsmod ] && UNUSED_MODULES=$(lsmod |grep '[[:space:]]0 $'| cut -d " " -f 1)
-  for module in ${UNUSED_MODULES}; do
-    rmmod $module
-  done
-
   # taken from Gluon (http://lists.infradead.org/pipermail/openwrt-devel/2020-October/031783.html)
   echo 3 > /proc/sys/vm/drop_caches
+
+  [ -x /sbin/lsmod ] || return
+  # remove unused kernel modules
+  PREV_MOD_COUNT=0
+  while true; do
+    UNUSED_MODULES=$(lsmod |grep '[[:space:]]0 $'| cut -d " " -f 1)
+    if ( $(${UNUSED_MODULES} | wc -w) -eq ${PREV_MOD_COUNT} ); then 
+      break
+    fi
+    for module in ${UNUSED_MODULES}; do
+      rmmod $module
+    done
+    PREV_MOD_COUNT=$(echo ${UNUSED_MODULES} | wc -w)
+  done
 }
 
 ffberlin_freeup_ram
